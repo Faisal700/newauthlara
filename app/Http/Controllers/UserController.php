5987b8e8ac1dea082b7548931b69d1c68ed95096
Faisal <faisalsandhu166@gmail.com>
2020-11-26 00:03:20 +0500

    asnkk

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,7 +19,7 @@ class UserController extends Controller
     	{
     		$avatar = $request->file('avatar');
     		$fileName = time().'.'. $avatar->getClientOriginalExtension();
-    		Image::make($request->file('avatar'))->resize(300,300)->save($fileName);
+    		$avatar->move(public_path('images'), $fileName);
     		$user = Auth::user();
     		$user->avatar = $fileName;
     		$user->save();
